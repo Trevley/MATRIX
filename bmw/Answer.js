175d67d63fa3455af310a4dd793df4708ab3ce28
Lil Trev <trevorleyian2@gmail.com>
2025-01-04 15:18:07 +0300

    Update Answer.js

--- bmw/Answer.js
+++ bmw/Answer.js
@@ -11,7 +11,7 @@ zokou({nomCom:"bot",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOption
   const {repondre,ms,arg}=commandeOptions;
   
     if(!arg || !arg[0])
-    {return repondre("yes I'm listening to you.")}
+    {return repondre("yes Matrix xmd is listening to you.")}
     //var quest = arg.join(' ');
   try{
     
